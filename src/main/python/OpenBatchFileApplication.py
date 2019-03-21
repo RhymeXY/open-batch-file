dc8d88f191360c5a09f8bc0c97ecf188b3e7d880
xinye1996 <18227665308@163.com>
2019-03-21 16:51:04 +0800

    feat: 引入日志，设置日志级别

--- src/main/python/OpenBatchFileApplication.py
+++ src/main/python/OpenBatchFileApplication.py
@@ -1,4 +1,8 @@
 from src.main.python.service.OpenBatch import OpenBatch
+import logging
+
+# 设置日志级别为DEBUG
+logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
     batch = OpenBatch()
